e9fe7866a32f1b836daeeb7ca44f9b65e06fe088
Dan Schlosser <dan.r.schlosser@gmail.com>
2015-06-27 12:38:14 -0700

    send intents to background

--- ext/src/inject/record.js
+++ ext/src/inject/record.js
@@ -1,26 +1,25 @@
-chrome.extension.sendMessage({type: 'record'}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+$('a, button, input, select, textarea').click(function(e) {
+	console.log(e);
+	chrome.extension.sendMessage({
+		type: 'record',
+		intent: {
+			intentType: 'click',
+			data: {
+				selector: e.target.selector,
+				innerText: e.target.innerText
+			}
+		}
+	}, function(response) {
+		var readyStateCheckInterval = setInterval(function() {
+			if (document.readyState === "complete") {
+				clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/record.js");
-		// ----------------------------------------------------------
+				// ----------------------------------------------------------
+				// This part of the script triggers when page is done loading
+				console.log("Hello. This message was sent from scripts/record.js");
+				// ----------------------------------------------------------
 
-	}
-	}, 10);
+			}
+		}, 10);
+	});
 });
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.type === 'record-start') {
-            var recognition = new webkitSpeechRecognition();
-            recognition.onresult = function(event) {
-              console.log(event)
-            }
-            recognition.start();
-            console.log('got message in record.js', request);
-        }
-    }
-);
