7c3f2c9324447d9e2b3b9a6ad4adf47ca20a6c11
Matthew Arbesfeld <arbesfeld@gmail.com>
2015-06-27 14:13:23 -0700

    Finish rebase

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,22 +1,5 @@
-<<<<<<< HEAD
-||||||| merged common ancestors
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-=======
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
 var pendingIntents = [];
 
->>>>>>> Add speech recording to the chrome extension
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
