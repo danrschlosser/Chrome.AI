9a60f879b59e75263c7cce367b0349a0b3e16a32
Dan Schlosser <dan.r.schlosser@gmail.com>
2015-06-27 16:20:47 -0700

    serialize json as a c function name

--- ext/src/inject/record.js
+++ ext/src/inject/record.js
@@ -1,3 +1,5 @@
+
+// Catch clicks
 $('a, button, input, select, textarea').click(function(e) {
 	chrome.extension.sendMessage({
 		type: 'record',
@@ -9,6 +11,25 @@ $('a, button, input, select, textarea').click(function(e) {
 			}
 		}
 	}, function(response) {
-		// After background.js response
+		// After background.js responds
 	});
 });
+
+// Catch typing
+$('input, select, textarea').change(function(e) {
+	console.log($(this), "changed:", e);
+	var message = {
+		type: 'record',
+		intent: {
+			intentType: 'input',
+			data: {
+				selector: e.target.selector,
+				value: $(this).val()
+			}
+		}
+	};
+	console.log(message);
+	chrome.extension.sendMessage(message, function(response) {
+		// After background.js reponds
+	})
+})
