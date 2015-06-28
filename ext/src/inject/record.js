067504a981eee5021611dade918f570fd5a7dd9b
Dan Schlosser <dan.r.schlosser@gmail.com>
2015-06-27 17:39:15 -0700

    better selectors

--- ext/src/inject/record.js
+++ ext/src/inject/record.js
@@ -1,12 +1,36 @@
+// Add the ability to get the unique selector of an element
+jQuery.fn.getSelector = function () {
+    if (this.length != 1) throw 'Requires one element.';
+    var path, node = this;
+    if (node[0].id) return '#' + node[0].id;
+    while (node.length) {
+        var realNode = node[0], name = realNode.localName;
+        if (!name) break;
+        name = name.toLowerCase();
+
+        var parent = node.parent();
+
+        var siblings = parent.children(name);
+        if (siblings.length > 1) {
+            name += ':eq(' + siblings.index(realNode) + ')';
+        }
+
+        path = name + (path ? '>' + path : '');
+        node = parent;
+    }
+
+    return path;
+};
 
 // Catch clicks
 $('a, button, input, select, textarea').click(function(e) {
+	var path = $(this).getSelector();
 	chrome.extension.sendMessage({
 		type: 'record',
 		intent: {
 			intentType: 'click',
 			data: {
-				selector: e.target.selector,
+				selector: path,
 				innerText: e.target.innerText
 			}
 		}
@@ -23,7 +47,7 @@ $('input, select, textarea').change(function(e) {
 		intent: {
 			intentType: 'input',
 			data: {
-				selector: e.target.selector,
+				selector: $(this).getSelector(),
 				value: $(this).val()
 			}
 		}
@@ -31,5 +55,5 @@ $('input, select, textarea').change(function(e) {
 	console.log(message);
 	chrome.extension.sendMessage(message, function(response) {
 		// After background.js reponds
-	})
-})
+	});
+});
